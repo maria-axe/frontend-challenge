05993147ce22d1414b0d199f0502e2154c8a7b5e
Maria Ferro <mariakarinesparks@outlook.com>
2024-09-12 20:54:26 -0300

    Add comments explaining the features

--- view/frontend/web/js/marvel-comics.js
+++ view/frontend/web/js/marvel-comics.js
@@ -23,10 +23,16 @@ define([
             this.loadMoreComics();
         },
 
+        /**
+         * Initializes the modal window for a given comic.
+         *
+         * @param {Object} comic - The comic object for which the modal window should be initialized.
+         */
         initModal: function(comic) {
             let modalElement = $('#custom-popup-modal-' + comic.id);
 
             if (modalElement.length) {
+                // The modal options
                 var options = {
                     type: 'popup',
                     responsive: true,
@@ -44,25 +50,41 @@ define([
                 var popup = modal(options, modalElement);
                 modalElement.modal('openModal');
             } else {
-                console.error('Elemento do modal não encontrado para o quadrinho com ID:', comic.id);
+                console.error('Elemento do modal nâo encontrado para o quadrinho com ID:', comic.id);
             }
         },
 
+        /**
+         * Marks 10% of the comics as rare.
+         *
+         * @param {array} comics - Array of comics to be marked as rare.
+         */
         markRareComics: function(comics) {
             const totalComics = comics.length;
             const rareCount = Math.floor(totalComics * 0.1); // 10% dos quadrinhos
             const rareIndices = new Set();
             
+            // Randomly select 10% of the comics and mark them as rare
             while (rareIndices.size < rareCount) {
                 const randomIndex = Math.floor(Math.random() * totalComics);
                 rareIndices.add(randomIndex);
             }
 
+            // Set the isRare property of the selected comics to true
             rareIndices.forEach(index => {
                 comics[index].isRare = true;
             });
         },
 
+        /**
+         * Loads more comics from the Marvel API.
+         * 
+         * This method loads more comics from the Marvel API and adds them to the
+         * comics collection. The comics are marked as rare randomly.
+         * 
+         * @fires loading
+         * @fires loaded
+         */
         loadMoreComics: function() {
             const timeStamp = '1726056711';
             const apiKey = '4c27e41d125dff1a975876b76ec7454d';
