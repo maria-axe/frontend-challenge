03e631fb70e70359c9238af7b1bdc2d8e169b908
Maria Ferro <mariakarinesparks@outlook.com>
2024-09-12 15:05:21 -0300

    Add loop to display the comics and modal

--- view/frontend/web/js/marvel-comics.js
+++ view/frontend/web/js/marvel-comics.js
@@ -6,25 +6,50 @@
 define([
     'ko',
     'uiComponent',
-    'jquery'
-], function (ko, Component, $) {
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function (ko, Component, $, modal) {
     'use strict';
 
     return Component.extend({
-        isVisible: ko.observable(true),
-        comics: ko.observable(''),
-        comicName: ko.observable(''),
+        isVisible: ko.observable(false),
+        comics: ko.observable([]),
 
         initialize: function () {
             this._super();
-            this.mavelComics();
+            this.marvelComics();
         },
 
-        mavelComics: function() {
+        initModal: function(comic) {
+            let modalElement = $('#custom-popup-modal-' + comic.id);
+
+            if (modalElement.length) {
+                var options = {
+                    type: 'popup',
+                    responsive: true,
+                    innerScroll: true,
+                    modalClass: 'custom-popup-modal',
+                    buttons: [{
+                        text: $.mage.__('Close'),
+                        class: '',
+                        click: function () {
+                            modalElement.modal('closeModal');
+                        }
+                    }]
+                };
+
+                var popup = modal(options, modalElement);
+                modalElement.modal('openModal');
+            } else {
+                console.error('Elemento do modal não encontrado para o quadrinho com ID:', comic.id);
+            }
+        },
+
+        marvelComics: function() {
             const timeStamp = '1726056711';
             const apiKey = '4c27e41d125dff1a975876b76ec7454d';
             const md5 = '222b5bd5511a9c667555a5d00e659418';
-        
+
             const self = this;
 
             $.ajax({
@@ -33,11 +58,12 @@ define([
                 cache: true
             })
             .done(function(response) {
-                console.log(response);
+                self.comics(response.data.results);
+                self.isVisible(true);
             })
             .fail(function() {
                 console.log('error');
             });
-        }            
+        }
     });
 });
