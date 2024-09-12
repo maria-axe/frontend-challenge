11ef7ee5f413f59a99803a3414cf28caea39fe87
Maria Ferro <mariakarinesparks@outlook.com>
2024-09-12 19:20:04 -0300

    Starting customization and page structuring settings

--- view/frontend/web/js/marvel-comics.js
+++ view/frontend/web/js/marvel-comics.js
@@ -13,11 +13,14 @@ define([
 
     return Component.extend({
         isVisible: ko.observable(false),
-        comics: ko.observable([]),
+        comics: ko.observableArray([]),
+        loading: ko.observable(true),
+        currentOffset: ko.observable(0),
+        limit: 20,
 
         initialize: function () {
             this._super();
-            this.marvelComics();
+            this.loadMoreComics();
         },
 
         initModal: function(comic) {
@@ -45,21 +48,41 @@ define([
             }
         },
 
-        marvelComics: function() {
+        markRareComics: function(comics) {
+            const totalComics = comics.length;
+            const rareCount = Math.floor(totalComics * 0.1); // 10% dos quadrinhos
+            const rareIndices = new Set();
+            
+            while (rareIndices.size < rareCount) {
+                const randomIndex = Math.floor(Math.random() * totalComics);
+                rareIndices.add(randomIndex);
+            }
+
+            rareIndices.forEach(index => {
+                comics[index].isRare = true;
+            });
+        },
+
+        loadMoreComics: function() {
             const timeStamp = '1726056711';
             const apiKey = '4c27e41d125dff1a975876b76ec7454d';
             const md5 = '222b5bd5511a9c667555a5d00e659418';
-
             const self = this;
+            const offset = self.currentOffset();
+            self.loading(true);
 
             $.ajax({
-                url: `https://gateway.marvel.com:443/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=10`,
+                url: `https://gateway.marvel.com:443/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=${self.limit}&offset=${offset}`,
                 method: 'GET',
                 cache: true
             })
             .done(function(response) {
-                self.comics(response.data.results);
+                const newComics = response.data.results;
+                self.markRareComics(newComics);
+                self.comics(self.comics().concat(newComics));
+                self.currentOffset(offset + self.limit);
                 self.isVisible(true);
+                self.loading(false);
             })
             .fail(function() {
                 console.log('error');
